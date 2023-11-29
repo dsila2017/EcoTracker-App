531cd3b74094b4d2c6121684f60444e2012e5013
dsila2017 <you@example.com>
2023-11-29 16:33:21 +0400

    Creating App

---
+++ EcoTracker App/EcoTracker App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EcoTracker App
+//
+//  Created by David on 11/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
