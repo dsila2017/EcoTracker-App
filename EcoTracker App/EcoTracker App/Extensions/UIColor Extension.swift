a22b3b22c2c5586c9971c014b1741925ebbc933f
dsila2017 <you@example.com>
2023-11-29 20:30:37 +0400

    Add Networking and UIColor Extension

---
+++ EcoTracker App/EcoTracker App/Extensions/UIColor Extension.swift
@@ -0,0 +1,32 @@
+//
+//  UIColor Extension.swift
+//  EcoTracker App
+//
+//  Created by David on 11/29/23.
+//
+
+import UIKit
+
+struct CustomColors {
+    static let primary = UIColor(hex: "#552CAD")
+    static let background = UIColor(hex: "#0A061A")
+    static let darkGray = UIColor(hex: "#231F31")
+    static let gray = UIColor(hex: "#83818A")
+    static let white = UIColor(hex: "#F3F1F7")
+}
+
+extension UIColor {
+    convenience init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+ 
+        var rgb: UInt64 = 0
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+ 
+        let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgb & 0x0000FF) / 255.0
+ 
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+}
