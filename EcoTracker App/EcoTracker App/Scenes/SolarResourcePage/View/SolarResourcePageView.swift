3f0025df26b8403ff8c052ccc55131dbbc4d37e6
Jamaტa <jamata94@gmail.com>
2023-11-30 17:33:18 +0400

    Create UI on Solar page
    Setup input field and button

--- EcoTracker App/EcoTracker App/Scenes/SolarResourcePage/View/SolarResourcePageView.swift
+++ EcoTracker App/EcoTracker App/Scenes/SolarResourcePage/View/SolarResourcePageView.swift
@@ -9,10 +9,69 @@ import UIKit
 
 class SolarResourcePageViewController: UIViewController {
 
+    private lazy var searchField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Search by address"
+        textField.backgroundColor = CustomColors.darkGray
+        textField.textColor = CustomColors.white
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.layer.cornerRadius = 10
+        textField.clipsToBounds = true
+        textField.textAlignment = .left
+        textField.tintColor = CustomColors.white
+        
+        setupTextField(textField)
+        
+        return textField
+    }()
+    
+    private let searchButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Search", for: .normal)
+        button.backgroundColor = CustomColors.primary
+        button.setTitleColor(CustomColors.white, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 10
+        button.clipsToBounds = true
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = CustomColors.background
+        setupViews()
     }
-
-
+    
+    private func setupTextField(_ textField: UITextField) {
+        if let placeholder = textField.placeholder {
+            textField.attributedPlaceholder = NSAttributedString(
+                string: placeholder,
+                attributes: [NSAttributedString.Key.foregroundColor: CustomColors.gray]
+            )
+        }
+        
+        let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: textField.frame.height))
+        textField.leftView = leftPaddingView
+        textField.leftViewMode = .always
+    }
+    
+    private func setupViews() {
+        view.addSubview(searchField)
+        view.addSubview(searchButton)
+        
+        NSLayoutConstraint.activate([
+            searchField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            searchField.topAnchor.constraint(equalTo: view.topAnchor, constant: 180),
+            searchField.widthAnchor.constraint(equalToConstant: 320),
+            searchField.heightAnchor.constraint(equalToConstant: 44),
+            
+            searchButton.topAnchor.constraint(equalTo: searchField.bottomAnchor, constant: 20),
+            searchButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            searchButton.widthAnchor.constraint(equalToConstant: 320),
+            searchButton.heightAnchor.constraint(equalToConstant: 56)
+        ])
+    }
 }
+
+
