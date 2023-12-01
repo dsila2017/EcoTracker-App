433181ab684da3bd6ff8383a6439f0a0f50facac
razmaa <145837511+razmaa@users.noreply.github.com>
2023-12-01 11:03:53 +0400

    Create Species Result ViewController

--- EcoTracker App/EcoTracker App/Scenes/SpeciePage/View/SpeciePageView.swift
+++ EcoTracker App/EcoTracker App/Scenes/SpeciePage/View/SpeciePageView.swift
@@ -7,27 +7,34 @@
 
 import UIKit
 
-class SpeciePageViewController: UIViewController {
+final class SpeciePageViewController: UIViewController {
     
     //MARK: - Properties
-    let cityNameTextField: UITextField = {
+    private let cityNameTextField: UITextField = {
         let textField = UITextField()
         textField.attributedPlaceholder = NSAttributedString(string: "Enter City Name", attributes: [NSAttributedString.Key.foregroundColor: CustomColors.gray] )
         textField.borderStyle = .roundedRect
         textField.textColor = CustomColors.white
         textField.backgroundColor = CustomColors.darkGray
         textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.layer.cornerRadius = 10
+        textField.clipsToBounds = true
         return textField
     }()
     
-    let fetchButton: UIButton = {
+    private let fetchButton: UIButton = {
         let button = UIButton()
-        button.setTitle("Fetch Species Info", for: .normal)
+        button.setTitle("Search", for: .normal)
         button.backgroundColor = CustomColors.primary
         button.layer.cornerRadius = 8
+        button.clipsToBounds = true
+        button.setTitleColor(CustomColors.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
+    
+    private let viewModel = SpeciePageViewModel()
     
     //MARK: - Life Cycle
     override func viewDidLoad() {
@@ -60,8 +67,27 @@ class SpeciePageViewController: UIViewController {
     
     private func addActionForButton() {
         fetchButton.addAction(UIAction(handler: { [weak self] _ in
-            let resultViewController = SpeciesResultView()
-            self?.present(resultViewController, animated: true)
+            guard let cityName = self?.cityNameTextField.text else {
+                return 
+            }
+
+            self?.viewModel.fetchCityID(for: cityName) { result in
+                switch result {
+                case .success(let fetchedCityId):
+                    DispatchQueue.main.async {
+                        self?.presentSecondView(with: fetchedCityId)
+                    }
+                case .failure(let error):
+                    // Handle error
+                    print("Error fetching city ID: \(error)")
+                }
+            }
         }), for: .touchUpInside)
+    }
+    
+    private func presentSecondView(with cityID: Int) {
+        let secondViewController = SpeciesResultView()
+        secondViewController.cityID = cityID
+        self.present(secondViewController, animated: true, completion: nil)
     }
 }
--- EcoTracker App/EcoTracker App/Scenes/SpeciePage/View/SpeciesResultView.swift
+++ EcoTracker App/EcoTracker App/Scenes/SpeciePage/View/SpeciesResultView.swift
@@ -7,23 +7,67 @@
 
 import UIKit
 
-class SpeciesResultView: UIViewController {
+final class SpeciesResultView: UIViewController {
 
+    var cityID: Int = 0
+    
+    private var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = CustomColors.background
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(SpeciesTableViewCell.self, forCellReuseIdentifier: "speciesCell")
+        return tableView
+    }()
+    
+    private var viewModel = SpeciesResultsViewModel()
+    private var species = [SpeciesInfo]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = CustomColors.background
-        // Do any additional setup after loading the view.
+        setupTableView()
+        fetchData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func fetchData(){
+        viewModel.fetchSpeciesInfo(for: cityID) { [weak self] result in
+            switch result {
+            case .success(let species):
+                self?.species = species
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            case .failure(let error):
+                print("Error fetching species info: \(error)")
+            }
+        }
     }
-    */
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
 
 }
+
+extension SpeciesResultView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        species.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "speciesCell", for: indexPath) as? SpeciesTableViewCell else {
+            fatalError("Could not dequeue NewsCell")
+        }
+        cell.configure(with: species[indexPath.row])
+        return cell
+    }
+}
+
+
