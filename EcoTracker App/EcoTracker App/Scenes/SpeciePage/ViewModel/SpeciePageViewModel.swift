cd69c76384a56aac71950ba702571440f961cca9
razmaa <145837511+razmaa@users.noreply.github.com>
2023-12-01 11:03:20 +0400

    Create Specie Page ViewModel

--- EcoTracker App/EcoTracker App/Scenes/SpeciePage/ViewModel/SpeciePageViewModel.swift
+++ EcoTracker App/EcoTracker App/Scenes/SpeciePage/ViewModel/SpeciePageViewModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import NetworkManagerPro
+
+final class SpeciePageViewModel {
+    private let network: Network
+
+    init(network: Network = Network()) {
+        self.network = network
+    }
+
+    func fetchCityID(for cityName: String, completion: @escaping (Result<Int, Error>) -> Void) {
+        guard let encodedCityName = cityName.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+              let url = URL(string: "https://api.inaturalist.org/v1/places/autocomplete?q=\(encodedCityName)") else {
+            completion(.failure(NetworkError.data))
+            return
+        }
+        
+        network.request(with: url) { (result: Result<cityData, Error>) in
+            switch result {
+            case .success(let cities):
+                if let firstCity = cities.results.first {
+                    completion(.success(firstCity.id))
+                } else {
+                    completion(.failure(NetworkError.parse))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
