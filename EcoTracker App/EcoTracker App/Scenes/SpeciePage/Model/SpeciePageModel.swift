d8d23d4dc7881d8c65cf07d302f795fd816550b1
razmaa <145837511+razmaa@users.noreply.github.com>
2023-12-01 11:02:44 +0400

    Create Specie Model

--- EcoTracker App/EcoTracker App/Scenes/SpeciePage/Model/SpeciePageModel.swift
+++ EcoTracker App/EcoTracker App/Scenes/SpeciePage/Model/SpeciePageModel.swift
@@ -7,14 +7,37 @@
 
 import Foundation
 
-struct City {
+struct cityData: Decodable {
+    let results: [City]
+}
+struct City: Decodable {
     let id: Int
     let name: String
 }
 
-struct SpeciesInfo {
-    let name: String
-    let imageURL: String
-    let uploaderName: String
-    let wikipediaLink: String?
+struct SpeciesResponse: Decodable {
+    let results: [SpeciesInfo]
 }
+
+struct SpeciesInfo: Decodable {
+    let taxon: TaxonInfo
+}
+
+struct TaxonInfo: Decodable {
+    let name: String
+    let defaultPhoto: PhotoInfo
+    let wikipediaURL: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case defaultPhoto = "default_photo"
+        case wikipediaURL = "wikipedia_url"
+    }
+
+}
+
+struct PhotoInfo: Decodable {
+    let attribution: String
+    let url: String
+}
+
